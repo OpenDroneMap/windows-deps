3e42dc82066b5f91616d49c6df6f7406f8ce368b
Charles Milette <charles.milette@gmail.com>
2026-04-17 23:23:16 -0400

    Remove sqlite3 from python ZIP

--- get_deps.py
+++ get_deps.py
@@ -8,11 +8,11 @@ def main():
     print("Downloading python zip...")
     python_zip_filename = python_zip.split('/')[-1]
     urllib.request.urlretrieve(python_zip, python_zip_filename)
-    print("...Complete. Removing python312._pth...")
+    print("...Complete. Removing python312._pth and sqlite3.dll...")
     with ZipFile(python_zip_filename, 'r') as zin:
-        with ZipFile(python_zip_filename[:-4] + "-less-pth.zip", 'w') as zout:
+        with ZipFile(python_zip_filename[:-4] + "-less-pth-sqlite.zip", 'w') as zout:
             for item in zin.infolist():
-                if item.filename != "python312._pth":
+                if item.filename != "python312._pth" and item.filename != "sqlite3.dll":
                     buffer = zin.read(item.filename)
                     zout.writestr(item, buffer)
     os.remove(python_zip_filename)
